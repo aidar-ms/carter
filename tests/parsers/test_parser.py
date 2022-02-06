6e48385205ff02768f0e3370f2a41790d5a674d8
Aidar Mamytov <mamytov1905@gmail.com>
2022-02-06 20:43:45 +0000

    Added commented code sample showing how to add html content to PageSnapshot

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,8 +1,9 @@
-import pdb
+
+
 from parsers.core import Parser
 
 
-def test_hello():
+def test_parse_spotify_blog():
     """ NOTE: Makes live HTTP requests
     """
     url = "https://engineering.atspotify.com/"
@@ -11,3 +12,16 @@ def test_hello():
 
     assert type(html) is str
     assert len(html) > 0
+
+
+# Code sample of saving html in PageSnapshot
+
+# from datetime import datetime
+# from db import get_session
+# from models.page_snapshot import PageSnapshot
+
+    # session = get_session()
+    # ts = datetime.now()
+    # ps = PageSnapshot(timestamp=ts, url=url, html_content=html)
+    # session.add(ps)
+    # session.commit()
