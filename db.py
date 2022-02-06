3c4c8dc359a4910506f267094ed917e7a37a51b2
Aidar Mamytov <mamytov1905@gmail.com>
2022-02-06 20:43:11 +0000

    Added get_session function

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
 from config import base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-
-engine = create_engine(base.SQLALCHEMY_DATABASE_URI)
+def get_session():
+    engine = create_engine(base.SQLALCHEMY_DATABASE_URI)
+    Session = sessionmaker(bind=engine)
+    return Session()
 
 # Base model
 Base = declarative_base()
