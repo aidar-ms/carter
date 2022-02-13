a235dae09096a561e821b6de13eeaede332e8522
Aidar Mamytov <mamytov1905@gmail.com>
2022-02-13 00:37:56 +0000

    Added postgres-to-postgresql substitution

--- config/base.py
+++ config/base.py
@@ -1,8 +1,10 @@
 import os
+import re
 from dotenv import load_dotenv
 
 if os.environ.get("ENVIRONMENT") == "dev":
     load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+# Substitution of "postgres" to "postgresql" is needed because SQLAlchemy is not happy with "postgres://"
+DATABASE_URL = re.sub(r"postgres\b", "postgresql", os.environ.get("DATABASE_URL"))
 REDIS_URL = os.environ.get("REDIS_URL")
